454c8012f7df593fbe5c20751d99a606eba6a326
Javier Jiménez <javijmor@gmail.com>
2015-05-15 16:04:08 +0200

    TYPO

--- testfiles/wracost_test1.php
+++ testfiles/wracost_test1.php
@@ -3,7 +3,7 @@
 if (!file_exists("blockage")){
     file_put_contents(rand(),"Testing");
 	$i = 1000;
-	while (--$i) echo $i; // Simulating big things happening in the server
+	while (--$i) echo $i; // Simulating big things happening on the server
     file_put_contents("blockage", "blocked");
 }else{
     echo "You dun goofed";
