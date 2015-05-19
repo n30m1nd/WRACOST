a83a4b8231da3996e203bd790ba0864817aa4198
Javier Jimenez <javijmor@gmail.com>
2015-05-19 20:00:29 +0200

    NEW: --params and --payloads arguments now work. Not with full functionality tho

--- core/arg_parser.py
+++ core/arg_parser.py
@@ -10,8 +10,8 @@ class ArgumentParser():
         self.parser = argparse.ArgumentParser(description='Web Race Condition and Stress Tester')
         self.args = self.parse_args()
 
-        if self.args.payload and not self.args.params:
-            exit("[-] Can't use --args without -p argument.")
+        if self.args.payloads and not self.args.params:
+            exit("[-] Can't use --params without --payloads argument.")
         if self.args.v >= 2:
             print "[i] There are only 2 verbosity levels, sorry."
         pass
@@ -20,9 +20,9 @@ class ArgumentParser():
         self.parser.add_argument('url', help='url to test')
         self.parser.add_argument('method', help='request method (http://www.w3.org/Protocols/HTTP/Methods.html).')
         self.parser.add_argument('threads', type=int, help='Number of threads/connections to use.')
-        self.parser.add_argument('-p', '--param', type=str, nargs='+', default=None,
+        self.parser.add_argument('--params', type=str, nargs='+', default=None,
                                  help='params to inject values into.')
-        self.parser.add_argument('--payload', type=str, nargs='+', default=None,
+        self.parser.add_argument('--payloads', type=str, nargs='+', default=None,
         help='''Values for the params -
         If the number of values doesn't match the number of params, the value assigned to each param will be randomized.''')
         self.parser.add_argument('-v', action='count', default=0, help='be verbose')
--- wracost.py
+++ wracost.py
@@ -6,20 +6,31 @@ import httplib
 from urlparse import urlparse
 import threading
 import sys
+import requests
 from WRACOST.core import arg_parser
+
 
 class WRACOST():
 
-    def __init__(self, url, method, verbosity, params=None, args=None):
+    def __init__(self, url, method, verbosity, params=None, payloads=None, cookie=None):
         #   Set the command line arguments  #
         self.arg_url = url
         self.arg_method = method
         self.arg_verbosity = verbosity
-        self.arg_param = params
-        self.arg_payload = args
+        self.arg_params = params
+        self.arg_payloads = payloads
+        self.arg_cookie = cookie
 
-        #   Useless if this class is used for any other programs    #
+        #   Useless if this class is used by any other files than this  #
         self.lock = threading.Lock()
+
+    def parse_param_data(self):
+            data_payload = {}
+            if (self.arg_params and self.arg_payloads):
+                for n, param in enumerate(self.arg_params):
+                    if (len(self.arg_params) >= len(self.arg_payloads)):
+                        data_payload[param] = self.arg_payloads[n % len(self.arg_payloads)]
+            return data_payload
 
     def do_request(self, method=None, url=None):
         if not (method):
@@ -27,35 +38,44 @@ class WRACOST():
         if not (url):
             url = self.arg_url
         try:
+            parsed_payload = self.parse_param_data()
+            req_sent = ''
+            #   POST or GET?    #
             #   We need to do as few things as we can here  #
-            parsed_url = urlparse(url)
-            connection = httplib.HTTPConnection(parsed_url.hostname, parsed_url.port)
-            connection.request(method, parsed_url.path)
+            if (method == 'POST'):
+                self.semaphore.acquire()
+                req_sent =requests.post(url, data=parsed_payload)
+            elif(method == 'GET'):
+                self.semaphore.acquire()
+                req_sent = requests.get(url)
+
             #   End of critical fastness hyperspeed section #
+
             self.lock.acquire()
-            sys.stdout.write("[+] Request sent ")
+            sys.stdout.write("[+]\tRequest sent ")
             if (arg_verbosity > 0):
-                response = connection.getresponse()
-                print "to: ", arg_url
-                print "[+]\tmethod:", arg_method
-                print "[+]\tresponse headers: "
-                for thing, thong in response.getheaders():
-                    print "[+]\t\t",thing,': ',thong
+                print "to:", arg_url
+                print "[+]\t\tmethod:", arg_method
+                if (self.arg_method!='GET'):
+                    print "[+]\t\tpayload:", parsed_payload
+            #    print "[+]\tresponse headers: "
+            #    for thing, thong in response.getheaders():
+            #        print "[+]\t\t", thing,': ', thong
             self.lock.release()
-            connection.close()
+
         except httplib.HTTPException as ex:
             self.lock.acquire()
-            print "[-] Connection error"
-            print "[-] Err info: ", ex.message
+            print "[-] Connection error: ", ex.message
             self.lock.release()
         except Exception as ex:
             self.lock.acquire()
-            print "[-] Error"
-            print "[-] Err info: ", str(ex)
+            print "[-] Error: ", str(ex)
             self.lock.release()
 
-    def run(self, lock):
+
+    def run(self, lock, semaphore):
         self.lock = lock
+        self.semaphore = semaphore
         self.do_request()
 
 if __name__ == '__main__':
@@ -65,22 +85,31 @@ if __name__ == '__main__':
     arg_nthreads = parser.args.threads
     arg_verbosity = parser.args.v
     arg_method = parser.args.method
-    arg_param = parser.args.param
-    arg_payload = parser.args.payload
+    arg_param = parser.args.params
+    arg_payload = parser.args.payloads
     #   End of setting the arguments    #
+
+    #   Init                            #
+    #   Need to add: Arg cookie         #
     wracost = WRACOST(arg_url, arg_method, arg_verbosity, arg_param, arg_payload)
 
-    print('[+] Starting requests')
-    #   Call arg_nthreads threads       #
+    print('[+] Starting requests...')
+
     if (arg_verbosity > 0):
             print '[+]\t\tURL: ', arg_url
             print '[+]\t\tThreads: ', arg_nthreads
-    else:
-        ':'
 
     threads = []
+    semaphore = threading.Semaphore(0)
+    lock = threading.Lock()
     for i in range(arg_nthreads):
-        lock = threading.Lock()
-        t = threading.Thread(target=wracost.run, args=(lock,))
+        t = threading.Thread(target=wracost.run, args=(lock, semaphore,))
         threads.append(t)
         t.start()
+
+    print '[+] Damn semaphores, how do they work...?'
+    # Wait for all threads to
+    __import__('time').sleep(5)
+
+    for i in range(arg_nthreads):
+        semaphore.release()
