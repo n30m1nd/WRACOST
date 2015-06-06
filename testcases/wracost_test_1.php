129c42360e1578af2e49485b69f2a7f5aae745e3
Javier Jimenez <javijmor@gmail.com>
2015-06-06 15:37:14 +0200

    Explaining headers better

--- testcases/wracost_test_1.php
+++ testcases/wracost_test_1.php
@@ -3,8 +3,8 @@
 /********************************************************************
  * Test 1: Race condition met when writing to disk.          		*
  * ---------------------------------------------------------------- *
- * This test will try to write to disk unless blockfile is present  *
- * a file called "blockage".                                        *
+ * This test will try to write to disk unless "blockage" is present *
+ * which is a file that pretends to be the lock for a database elem.*
  *                                                                  *
  * WRACOST Testing command:                                         *
  * wracost.py http://localhost:80//testcases/wracost_test_1.php     *
